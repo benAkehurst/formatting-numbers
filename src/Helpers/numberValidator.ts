49f7976514155ac5278c3a2c161a96d32f07e9f4
benakehurst <benakehurst@hotmail.com>
2020-05-30 16:23:22 +0100

    feat(numberValidator): add wip file

---
+++ src/Helpers/numberValidator.ts
@@ -0,0 +1,7 @@
+const numberValidator = (phoneNumber: string) => {
+  // split string
+  // check first 2 numbers after + to match 44
+  // if they dont match return false
+  // else return true
+  return
+}
