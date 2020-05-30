214e66baca5bd0e5ede58f31ec34777f56fdb13a
benakehurst <benakehurst@hotmail.com>
2020-05-30 17:13:37 +0100

    feat(display): add class to show error message in red

--- src/Components/Display/Display.tsx
+++ src/Components/Display/Display.tsx
@@ -2,7 +2,13 @@ import * as React from 'react';
 import classes from './Display.module.scss';
 
 const display = (props: any) => {
-  return <div className={classes.DisplayWrapper}>{props.children}</div>;
+  return (
+    <div
+      className={[classes.DisplayWrapper, classes[props.displayType]].join(' ')}
+    >
+      {props.children}
+    </div>
+  );
 };
 
 export default display;
