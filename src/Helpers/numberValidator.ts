5a466767f9a5371b4e26703c885c586367ac8794
benakehurst <benakehurst@hotmail.com>
2020-05-30 17:12:55 +0100

    fix(numberValidator): fix bug when entering 444

--- src/Helpers/numberValidator.ts
+++ src/Helpers/numberValidator.ts
@@ -1,7 +1,7 @@
 const numberValidator = (phoneNumber: string) => {
   const numberToCheck = phoneNumber.split('');
-  const output = [numberToCheck[1], numberToCheck[2]].join('');
-  if (output !== '44') {
+  const output = [numberToCheck[0], numberToCheck[1], numberToCheck[2]].join('');
+  if (output !== '+44') {
     return false
   } else {
     return true;
