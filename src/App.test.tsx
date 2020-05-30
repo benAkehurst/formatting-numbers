e9b61d95f5ddf54307a7eacc07b1a5c60bc5ea1a
benakehurst <benakehurst@hotmail.com>
2020-05-30 15:16:32 +0100

    feat(app): initial commit

---
+++ src/App.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App from './App';
+
+configure({ adapter: new Adapter() });
+
+describe('<App />', () => {
+  let wrapper: HTMLElement;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it('should render the compoent', () => {
+    expect(wrapper).toBeTruthy();
+  });
+});
