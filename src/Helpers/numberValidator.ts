269e727a6e1e13fa554a70d0c70dea1a4a025de8
benakehurst <benakehurst@hotmail.com>
2020-05-31 10:45:13 +0100

    fix(bug): error when entering +44 only

--- src/Helpers/numberValidator.ts
+++ src/Helpers/numberValidator.ts
@@ -3,6 +3,8 @@ const numberValidator = (phoneNumber: string) => {
   const output = [numberToCheck[0], numberToCheck[1], numberToCheck[2]].join('');
   if (output !== '+44') {
     return false
+  } else if (output === '+44') {
+    return false;
   } else {
     return true;
   }
