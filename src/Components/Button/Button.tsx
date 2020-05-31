36da784f5c3e157b50132c39135553a57fd21ed4
benakehurst <benakehurst@hotmail.com>
2020-05-31 09:27:15 +0100

    feat(button): formatting of button component

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -3,7 +3,11 @@ import classes from './Button.module.scss';
 
 const button = (props: any) => {
   return (
-    <button className={classes.ButtonWrapper} onClick={props.clicked}>
+    <button
+      className={classes.ButtonWrapper}
+      onClick={props.clicked}
+      disabled={props.disabled}
+    >
       {props.children}
     </button>
   );
